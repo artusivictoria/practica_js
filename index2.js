09b36010249bbb736c165dcfbe3c23db9e78baa5
artusivictoria <vartusi@udesa.edu.ar>
2024-05-15 20:32:19 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,9 @@
+let fruits = ["banana", "naranja" , "limon" ,"manzana" , "mango"];
+let citrics = ["naranja" , "limon"];
+console.log(fruits);
+console.log(citrics);
+console.log(fruits.length);
+console.log(citrics.length);
+fruits.push("frutilla" , "melon")
+console.log(fruits);
+console.log(fruits.length);
